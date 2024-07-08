d731585bb37f72544ca880dc759f3b3469eb9a8b
ahmet <ahmet@DESKTOP-M9Q77CF>
2024-07-08 16:42:36 +0300

    Add project files.

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,19 @@
+﻿using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    internal class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Scott";
+            CurrentPlayer.Gold = 1000000;
+
+
+        }
+
+    }
+}
